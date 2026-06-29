5e20b1558f2031939be515dedfb7f31f546fe64d
Mokarama <mokaramaaktershanta@gmail.com>
2026-06-29 17:20:06 +0600

    added database models and auto migration

---
+++ models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+
+	Name     string `gorm:"type:varchar(100);not null"`
+	Email    string `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password string `gorm:"type:text;not null"`
+	Role     string `gorm:"type:varchar(20);default:driver"`
+}
